dfe17c5247f37bcd50cca76858294f4daf583f48
yuk-krhs <yuk.krhs@gmail.com>
2024-03-19 11:01:47 +0900

    Ｐアイドル、Ｓキャラ

--- docs/prism.js
+++ docs/prism.js
@@ -1,26 +1,34 @@
 let cookies = null;
 
-function parse_cookies() {
-    cookies = document.cookie.split(';').map(cookie => cookie.split('=')).reduce((accumulator, [key, value]) => ({...accumulator, [decodeURIComponent(key.trim())]: decodeURIComponent(value)}), {});
+document.addEventListener('keydown', function(event) { if(event.key === 'Escape') history.back(); });
+
+function parse_cookies()
+{
+    cookies = document.cookie.split(';')
+        .map(cookie => cookie.split('='))
+        .reduce((accumulator, [key, value]) => ({...accumulator, [decodeURIComponent(key.trim())]: decodeURIComponent(value)}), {});
 }
 
-function set_cookie(key, value) {
-    let date = new Date();
+function set_cookie(key, value)
+{
+    let date        = new Date();
     date.setFullYear(date.getFullYear() + 1);
     document.cookie = `${encodeURIComponent(key)}=${encodeURIComponent(value)};expires=${date.toUTCString()};path=/`;
-    cookies[key]= String(value);
+    cookies[key]    = String(value);
 
     //console.log('[' + key + ']=' + String(value));
 }
 
-function get_cookie(key, defaultValue) {
-    if (cookies === null) {
+function get_cookie(key, defaultValue)
+{
+    if(cookies === null)
         parse_cookies();
-    }
+
     return cookies[key] || defaultValue;
 }
 
-function rhythm_display(b) {
+function rhythm_display(b)
+{
     //console.log('rhythm_display=' + b);
 
 	const visible   = String(b) == 'true' ? 'tr.list.rhythm.level' : 'tr.list.rhythm.notes';
@@ -32,4 +40,67 @@ function rhythm_display(b) {
 	set_cookie('rhythm_label', String(b));
 
 	return false;
+}
+
+function toggleTableCells(visibleDisps, visibleStars, visibleObtains, visibleUnits)
+{
+    visibleUnits.push('.unit-0');
+
+    var selector1   = visibleDisps  .join(',');
+    var selector2   = visibleStars  .join(',');
+    var selector3   = visibleObtains.join(',');
+    var selector4   = visibleUnits  .join(',');
+
+    $('tr th').each(function() {
+        if(!$(this).is(selector1))
+            $(this).addClass('removed-cell');
+    });
+
+    $('tr.content td').each(function() {
+        if(!$(this).is(selector1)
+        || !$(this).is(selector2)
+        || !$(this).is(selector3)
+        || !$(this).is(selector4))
+            $(this).addClass('removed-cell');
+    });
+}
+
+function getVisibleDisps()      { return [ '.disp-0', $("#disp-selector input[name='disp']:checked").val() ]; }
+function getVisibleUnits()      { return $($('#unit-checker'))  .find('input[name="unit"]:checked')  .map(function() { return $(this).val(); }).get(); }
+function getVisibleObtainss()   { return $($('#obtain-checker')).find('input[name="obtain"]:checked').map(function() { return $(this).val(); }).get(); }
+function getVisibleStars()      { return $($('#star-checker'))  .find('input[name="star"]:checked')  .map(function() { return $(this).val(); }).get(); }
+function getVisibleRarities()   { return $($('#rarity-checker')).find('input[name="rarity"]:checked').map(function() { return $(this).val(); }).get(); }
+
+function restructPIdolTable(th)
+{
+    const id = '#pidol-list';
+
+    resetVisible(id);
+    toggleTableCells(getVisibleDisps(), getVisibleStars(), getVisibleObtainss(), getVisibleUnits());
+
+    if(th)
+        sortTable(th);
+    else
+        doSortTable($(id), lastSortColumnId, lastSortOrder);
+
+    mergeTableRows(id);
+
+    // todo クッキーに選択状態を保存
+}
+
+function restructSCharaTable(th)
+{
+    const id = '#schara-list';
+
+    resetVisible(id);
+    toggleTableCells([ ".disp-0" ], getVisibleRarities(), getVisibleObtainss(), getVisibleUnits());
+
+    if(th)
+        sortTable(th);
+    else
+        doSortTable($(id), lastSortColumnId, lastSortOrder);
+
+    mergeTableRows(id);
+
+    // todo クッキーに選択状態を保存
 }
--- docs/sortable-table.js
+++ docs/sortable-table.js
@@ -1,26 +1,44 @@
+var lastSortColumnId    = 5;
+var lastSortOrder       = 'desc';
+
+function resetVisible(id)
+{
+    id = id.startsWith('#') ? id : '#' + id;
+
+    $(id).find(".removed-cell").each(function() {
+        $(this).removeClass('removed-cell');
+        $(this).attr('rowspan', 1);
+    });
+};
+
 function sortTable(th)
 {
-    const table= th.closest("table");
-
-    table.find("td.removed-cell").each(function() { $(this).removeClass('removed-cell'); });
-
-    const columnId  = th.data('id');
-    const sortOrder = th.hasClass('asc') ? 'desc' : 'asc';
+    const table         = th.closest("table");
+    const columnId      = th.data('id');
+    const sortOrder     = th.hasClass('asc') ? 'desc' : 'asc';
 
     doSortTable(table, columnId, sortOrder);
 
     th.toggleClass('asc',  sortOrder === 'asc');
     th.toggleClass('desc', sortOrder === 'desc');
+
+    lastSortColumnId    = columnId;
+    lastSortOrder       = sortOrder;
 }
 
 function doSortTable(table, columnId, sortOrder)
 {
     const rows = table.find("tr.content");
 
-    rows.sort((a, b) =>
-    {
-        const aValue = $(a).find(`td[data-id="${columnId}"]`).data('value');
-        const bValue = $(b).find(`td[data-id="${columnId}"]`).data('value');
+    rows.sort((a, b) => {
+        const aElem  = $(a).find(`td[data-id="${columnId}"]`);
+        const bElem  = $(b).find(`td[data-id="${columnId}"]`);
+
+        if(aElem.css('display') == 'none')  return 1;
+        if(bElem.css('display') == 'none')  return -1;
+
+        const aValue = aElem.data('value');
+        const bValue = bElem.data('value');
 
         if($.isNumeric(aValue) && $.isNumeric(bValue))
             return sortOrder === 'asc' ? aValue - bValue : bValue - aValue;
@@ -31,7 +49,16 @@ function doSortTable(table, columnId, sortOrder)
     rows.detach().appendTo(table);
 }
 
-function mergeTableRows(table, columnId)
+function mergeTableRows(id)
+{
+    id = id.startsWith('#') ? id : '#' + id;
+
+    $(id).find("th.mergeable").each(function() {
+        doMergeTableRows($(id), $(this).data('id'));
+    });
+}
+
+function doMergeTableRows(table, columnId)
 {
     const rows  = table.find("tr.content");
     var count   = 0;
@@ -40,22 +67,28 @@ function mergeTableRows(table, columnId)
     {
         const curRow    = rows[i];
         const curCol    = $(curRow).find(`td[data-id="${columnId}"]`);
-        const aValue    = curCol.text();
+        const aValue    = curCol.css('display') + curCol.text();
         count           = 1;
 
-        for(var j= i + 1; j < rows.length; ++j)
+        if($(curCol).css('display') != 'none')
         {
-            const nextRow   = rows[j];
-            const nextCol   = $(nextRow).find(`td[data-id="${columnId}"]`);
-            const bValue    = nextCol.text();
+            for(var j= i + 1; j < rows.length; ++j)
+            {
+                const nextRow   = rows[j];
+                const nextCol   = $(nextRow).find(`td[data-id="${columnId}"]`);
+                const bValue    = nextCol.css('display') + nextCol.text();
 
-            if(aValue != bValue)
-                break;
+                if(aValue != bValue)
+                    break;
 
-            nextCol.addClass('removed-cell');
-            ++count;
+                nextCol.addClass('removed-cell');
+                ++count;
+            }
+
+            curCol.attr('rowspan', count);
+        } else
+        {
+            curCol.attr('rowspan', 1);
         }
-
-        curCol.attr('rowspan', count);
     }
 }
